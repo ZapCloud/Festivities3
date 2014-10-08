2ba59e3ef8eb7345a85897d132547717d475c9b0
Sam Sartor <eeky.sam@gmail.com>
2014-10-07 18:21:53 -0600

    Mess With States
    Try to fix bug (discovered to be in FML)

--- src/main/java/com/zapcloudstudios/festivities3/block/BlockCandyLog.java
+++ src/main/java/com/zapcloudstudios/festivities3/block/BlockCandyLog.java
@@ -6,7 +6,10 @@ import net.minecraft.block.properties.PropertyBool;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 
 public class BlockCandyLog extends BlockLog
 {
@@ -15,24 +18,27 @@ public class BlockCandyLog extends BlockLog
 	public BlockCandyLog()
 	{
 		super();
-		this.setDefaultState(this.blockState.getBaseState().withProperty(AXIS_PROP, BlockLog.EnumAxis.Y).withProperty(MELT_PROP, false));
+		this.setDefaultState(this.blockState.getBaseState().withProperty(AXIS_PROP, BlockLog.EnumAxis.Y).withProperty(MELT_PROP, Boolean.FALSE));
 		this.setCreativeTab(CreativeTabs.tabBlock);
 	}
 
+	@Override
 	public IBlockState getStateFromMeta(int meta)
 	{
-		return this.getDefaultState().withProperty(MELT_PROP, ((meta >> 2) & 1) != 0).withProperty(AXIS_PROP, BlockLog.EnumAxis.values()[meta & 3]);
+		return this.getDefaultState().withProperty(MELT_PROP, Boolean.valueOf((meta & 1) > 0)).withProperty(AXIS_PROP, BlockLog.EnumAxis.values()[(meta >> 2) & 3]);
 	}
 
+	@Override
 	public int getMetaFromState(IBlockState state)
 	{
 		int i = 0;
-		i += ((Boolean) state.getValue(MELT_PROP)) ? 1 : 0;
-		i <<= 2;
 		i += ((BlockLog.EnumAxis) state.getValue(AXIS_PROP)).ordinal();
+		i <<= 2;
+		i += ((Boolean) state.getValue(MELT_PROP)).booleanValue() ? 1 : 0;
 		return i;
 	}
 
+	@Override
 	protected BlockState createBlockState()
 	{
 		return new BlockState(this, new IProperty[] { AXIS_PROP, MELT_PROP });
