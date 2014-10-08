0fcdab4eac1c45c9a47052300485f31f1415a395
Sam Sartor <eeky.sam@gmail.com>
2014-10-07 18:49:42 -0600

    Fixed It
    Make blocks work

--- src/main/java/com/zapcloudstudios/festivities3/FestiveBlocks.java
+++ src/main/java/com/zapcloudstudios/festivities3/FestiveBlocks.java
@@ -1,6 +1,9 @@
 package com.zapcloudstudios.festivities3;
 
+import java.util.Iterator;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import com.zapcloudstudios.festivities3.block.BlockCandyLog;
@@ -17,5 +20,18 @@ public class FestiveBlocks
 	public static void registerBlocks()
 	{
 		GameRegistry.registerBlock(candyLog, "candy_log");
+		registerBlockStates(candyLog);
+	}
+	
+	public static void registerBlockStates(Block block)
+	{
+		Iterator iterator1 = block.getBlockState().getValidStates().iterator();
+
+        while (iterator1.hasNext())
+        {
+            IBlockState iblockstate = (IBlockState)iterator1.next();
+            int i = Block.blockRegistry.getIDForObject(block) << 4 | block.getMetaFromState(iblockstate);
+            Block.BLOCK_STATE_IDS.put(iblockstate, i);
+        }
 	}
 }
